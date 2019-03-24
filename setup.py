b7d860faf953ac76547e029a330cd5648b459955
fkhan <fkhan@botminds.com>
2019-03-24 19:57:26 +0530

    add gitignore, requirements and setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='codeml',
+    version='0.0.1',
+    description='textclassification with allennlp source code',
+    url='example.com',    
+    packages=find_packages(),
+)
